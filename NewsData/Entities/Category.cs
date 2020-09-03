2765df3c4f8c1761189235c31a4a3cc10600bbd2
17110143@student.hcmute.edu.vn <17110143@student.hcmute.edu.vn>
2020-09-03 15:34:10 +0700

    New update in data

---
+++ NewsData/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using NewsData.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsData.Entities
+{
+    public class Category
+    {
+        public int ID { set; get; }
+
+        public int SortOrder { set; get; }
+        
+        public  bool IsShowOnHome { set; get; }
+
+        public int? ParentID { set; get; }
+
+        public Status Status { set; get; }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ASP.NET Core 3.1 project from Hoang
 ## Technologi
 -Asp.Net Core MVC
+##Install package
+-Microsoft.EntityFrameworkCore
+-Microsoft.EntityFrameworkCore.Design
+-Microsoft.EntityFrameworkCore.Tool
+-Microsoft.EntityFrameworkCore.SqlServer
 ##Demo
