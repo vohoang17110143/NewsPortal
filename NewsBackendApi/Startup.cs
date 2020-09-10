73555e655416befc94c88d56036814faae9ce044
17110143@student.hcmute.edu.vn <17110143@student.hcmute.edu.vn>
2020-09-10 13:59:16 +0700

    Swagger update in BackEnd

--- NewsBackendApi/Startup.cs
+++ NewsBackendApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using News.Utilities.Constants;
 using NewsApplication.Catalog;
 using NewsApplication.Catalog.Product;
@@ -34,6 +35,11 @@ namespace NewsBackendApi
             //Declare DI
             services.AddTransient<IPublicProductService, PublicProductService>();
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop Solution", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +61,13 @@ namespace NewsBackendApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger eShopSolution V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
